7655f716065520a30d4aa110bdc336ed14b0ce25
Maria Soage <mariasoage@MacBook-Pro-de-Maria.local>
2020-03-17 23:29:00 +0100

    add select categories

--- ecommerce/src/app/Product/add/add.component.ts
+++ ecommerce/src/app/Product/add/add.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
-import {Router} from "@angular/router";
+import { Router } from '@angular/router';
 import { ProductService } from 'src/app/Service/product.service';
-
+import { TagService } from 'src/app/Service/tag.service';
+import { CategoryService } from 'src/app/Service/category.service';
 
 @Component({
   selector: 'app-add-product',
@@ -10,33 +11,59 @@ import { ProductService } from 'src/app/Service/product.service';
   styleUrls: ['./add.component.scss']
 })
 export class AddComponent implements OnInit {
-
-  constructor(private formBuilder: FormBuilder, private router: Router, private productService: ProductService) { }
-
   addForm: FormGroup;
+  //On declare la variable qui sert aux checkboxs
+  tagsData = [];
+  category = [];
 
-  // tags = this.productService.get();
-
-  ngOnInit() {
-
+  constructor(
+    private formBuilder: FormBuilder, 
+    private router: Router, 
+    private productService: ProductService,
+    private categoryService: CategoryService, 
+    private tagService: TagService) 
+    {
     this.addForm = this.formBuilder.group({
       id: [],
       name: new FormControl(''),
       price: new FormControl(''),
       photo: new FormControl(''),
       category: new FormControl(''),
-      // tags: new FormArray([
-      //   new FormControl(true),
-      //   new FormControl(false),
-      // ])
-    })
+      tags: new FormArray([])
+    });
+  }
+
+  ngOnInit() {
+    this.getTags();
+    this.getCategories();
+  }
+
+  getTags() {
+    this.tagService.getTags()
+      .subscribe(data => {
+        this.tagsData = data;
+        this.addCheckBoxes();
+      });
+  }
+
+  addCheckBoxes() {
+    this.tagsData.forEach((o, i) => {
+      const control = new FormControl(i === 0); // if first item set to true, else false
+      (this.addForm['controls'].tags as FormArray).push(control);
+    });
+  }
+
+  getCategories() {
+    this.categoryService.getCategories()
+      .subscribe(data => {
+        this.category = data;
+      });
   }
 
   onSubmit() {
     this.productService.addProduct(this.addForm.value)
-      .subscribe( data => {
+      .subscribe(data => {
         this.router.navigate(['product']);
       });
   }
-
 }
